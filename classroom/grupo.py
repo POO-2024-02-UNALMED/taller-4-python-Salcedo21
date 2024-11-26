1dfe3bc06d8aa78606776be6f5bb7eb02134be86
Santiago Salcedo <ssalcedor@unal.edu.co>
2024-11-26 09:35:19 -0500

    Update grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -18,7 +18,7 @@ class Grupo:
         nueva_lista.append(alumno) 
         self.listadoAlumnos += nueva_lista 
     def __str__(self):
-        return "Grupo de estudiantes:" + self._grupo 
+        return "Grupo de estudiantes: " + self._grupo 
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 10"):
